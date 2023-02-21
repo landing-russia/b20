60f02b6b01d2ccc4f69104dd8115589502df37c5
landing-russia <landing-russia@yandex.ru>
2023-02-21 23:45:58 +0300

    only @astrojs/image & @astrojs/mdx added

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config"
+import image from "@astrojs/image"
+import mdx from "@astrojs/mdx"
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    mdx(),
+  ],
+})
---
+++ src/content/config.ts
@@ -0,0 +1,13 @@
+import { z, defineCollection } from "astro:content"
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    tags: z.array(z.string()),
+  }),
+})
+
+export const collections = {
+  blog: blogCollection,
+}
