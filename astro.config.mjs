7d976cf12d35a78ea69d306ea28e00acd1cba66d
landing-russia <landing-russia@yandex.ru>
2023-03-12 22:08:37 +0300

    deploy begin 8

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
 import { defineConfig } from "astro/config"
 import image from "@astrojs/image"
 import mdx from "@astrojs/mdx"
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/serverless"
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://landing-russia.github.io",
-  base: "/b20",
   integrations: [
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
     mdx(),
-    tailwind({ config: { applyBaseStyles: false } }),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
   ],
+  output: "server",
+  adapter: vercel(),
 })
